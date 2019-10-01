84e89735aee5dfc6216ec40ba846d2a4bd8b3363
Gustavo Alves <gustavo@gustavoalves.com.br>
2019-09-30 21:40:11 -0300

    Criando gráfico de linhas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'grafico-com-angular';
+export class AppComponent implements OnInit {
+  @ViewChild("meuCanvas", { static: true }) elemento: ElementRef;
+
+  ngOnInit(){
+    new Chart(this.elemento.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"],
+        datasets: [
+          {
+            data: [85,72,86,81,84,86,94,60,62,65,41,58],
+            borderColor: '#00AEFF',
+            fill: false
+          },
+          {
+            data: [33,38,10,93,68,50,35,29,34,2,62,4],
+            borderColor: "#FFCC00",
+            fill: false
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        }
+      }
+    });
+  }
 }
